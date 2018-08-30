3ac1ffe1937278f6fd2b9034d428fc6f5dcfc5c3
joe <kinjoyuji@gmail.com>
2018-08-30 19:35:55 +0900

    firebaseを読み込む

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// ここから
+import firebase from 'firebase';
+import firebaseConfig from './config/firebase';
+
+firebase.initializeApp(firebaseConfig);
+
+// ここまで追記
+
 class App extends Component {
   render() {
     return (
@@ -13,7 +21,6 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        {/* ここから */}
         <div class="form">
           <form class="register-form">
             <input type="text" placeholder="email address"/>
@@ -21,7 +28,6 @@ class App extends Component {
             <button>Sign Up</button>
           </form>
         </div>
-        {/* ここまで追記 */}
       </div>
     );
   }
